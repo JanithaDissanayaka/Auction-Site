911a25f43db46e2289e0809975c226061fc9a8bf
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2025-10-13 13:19:56 +0530

    admin dashboard

--- Project 1/obj/Debug/net8.0/Project 1.AssemblyInfo.cs
+++ Project 1/obj/Debug/net8.0/Project 1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbe686e998444216914948b2129b6a9fdc2a29d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bab7c3d399b852195190bd2d635569ea4aa814e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
